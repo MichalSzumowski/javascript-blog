0460dfee090c6590591d4b68ea713261ffe05bfd
Michał Szumowski <mic.szumowski@gmail.com>
2021-04-27 13:12:52 +0200

    Add generateTitleLinks functionality - Excersie 6.4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+{
 
 /* document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
@@ -38,8 +39,49 @@
 
 }
   
-  const links = document.querySelectorAll('.titles a');
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  /* for each article */
+    for(let article of articles){
+
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element and get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into titleList */
+    html  = html + linkHTML;
+    }
+    
+  titleList.innerHTML = html;
   
+  const links = document.querySelectorAll('.titles a');
+
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+
+generateTitleLinks();
+
+
+
+
+}
